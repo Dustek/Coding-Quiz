e5ed2708e5d1f23873f0830ef208f6e8f4a89b0b
Vytas <v.venckas@gmail.com>
2024-01-14 14:43:41 +0000

    added questions in javascript file

---
+++ js/questions.js
@@ -0,0 +1,29 @@
+const quizQuestions = [
+    {
+      question: "What does HTML stand for?",
+      choices: ["Hyper Text Markup Language", "Highly Typed Machine Learning", "Hyper Transferable Language", "Highly Technological Modern Language"],
+      correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+      question: "Which HTML tag is used to create a hyperlink?",
+      choices: ["<a>", "<link>", "<href>", "<url>"],
+      correctAnswer: "<a>",
+    },
+    {
+      question: "In JavaScript, what is the purpose of the 'var' keyword?",
+      choices: ["Declare a variable", "Create a loop", "Define a function", "Link to an external script"],
+      correctAnswer: "Declare a variable",
+    },
+    {
+      question: "Which CSS property is used to change the text color of an element?",
+      choices: ["color", "text-color", "font-color", "foreground-color"],
+      correctAnswer: "color",
+    },
+    {
+      question: "What does the 'console.log()' function do in JavaScript?",
+      choices: ["Prints text to the console", "Opens a new console window", "Logs errors to the console", "Creates a popup message"],
+      correctAnswer: "Prints text to the console",
+    },
+  ];
+
+  export default quizQuestions;
