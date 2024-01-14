b3e31cb54c99ac184db41bd3e7e4eee8460a9ed0
Vytas <v.venckas@gmail.com>
2024-01-14 16:19:32 +0000

    added answerclick and display questions logic

--- js/logic.js
+++ js/logic.js
@@ -4,24 +4,72 @@ var QuestionContainerElement = document.getElementById("questions");
 var ChoicesElement = document.getElementById("choices");
 var EndElement = document.getElementById("end-screen");
 var FeedbackElement = document.getElementById("feedback");
+var questionTitle = document.getElementById('question-title')
+var choicesContainer = document.getElementById('choices')
 
-
+import quizQuestions from './questions.js';
 
 
 var currentQuestionIndex = 0;
 var timer;
 var timerCount; 
 
+//Displays question and answer buttons
+function displayQuestion() {
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionTitle.textContent = currentQuestion.question;
+    // Clear previous choices
+    choicesContainer.innerHTML = '';
+    for (let index = 0; index < currentQuestion.choices.length; index++) {
+        var choice = currentQuestion.choices[index];
+        var button = document.createElement('button');
+        button.textContent = choice;
+        button.addEventListener('click', function () {
+          handleAnswerClick(choice);
+        });
+        choicesContainer.appendChild(button);
+      }
+}
+
+function handleAnswerClick(selectedAnswer) {
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+  
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      // Handle correct answer
+      console.log("Correct!");
+    } else {
+      // Handle incorrect answer (subtract time, etc.)
+      console.log("Incorrect!");
+    }
+    currentQuestionIndex++;
+    // Checks if there are more questions
+    if (currentQuestionIndex < quizQuestions.length) {
+      displayQuestion();
+    } else {
+      endQuiz();
+    }
+  }
+
+
+
+
+
 function startGame() {
     isWin = false;
-    timerCount = 10;
+    timerCount = 100;
     // Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
+    //TODO figure out this function
     renderBlanks()
     startTimer()
   }
 
 
+
+
+
+
+// Starts timer
   function startTimer() {
     timer = setInterval(function() {
       timerCount--;
