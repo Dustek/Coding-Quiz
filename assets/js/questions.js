a3b12ca39231fdc720a1cfb493f78924f9286ca8
Vytas <v.venckas@gmail.com>
2024-01-15 20:48:26 +0000

    mixed up question ordering

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,22 +6,22 @@ var quizQuestions = [
     },
     {
       question: "Which HTML tag is used to create a hyperlink?",
-      choices: ["<a>", "<link>", "<href>", "<url>"],
+      choices: ["<url>", "<link>", "<href>", "<a>"],
       correctAnswer: "<a>",
     },
     {
       question: "In JavaScript, what is the purpose of the 'var' keyword?",
-      choices: ["Declare a variable", "Create a loop", "Define a function", "Link to an external script"],
+      choices: ["Create a loop", "Declare a variable", "Define a function", "Link to an external script"],
       correctAnswer: "Declare a variable",
     },
     {
       question: "Which CSS property is used to change the text color of an element?",
-      choices: ["color", "text-color", "font-color", "foreground-color"],
+      choices: ["foreground-color", "text-color", "font-color", "color"],
       correctAnswer: "color",
     },
     {
       question: "What does the 'console.log()' function do in JavaScript?",
-      choices: ["Prints text to the console", "Opens a new console window", "Logs errors to the console", "Creates a popup message"],
+      choices: ["Logs errors to the console", "Opens a new console window", "Prints text to the console", "Creates a popup message"],
       correctAnswer: "Prints text to the console",
     },
   ];
